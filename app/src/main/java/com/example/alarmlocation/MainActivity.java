369ed21d6d334d1c007f47de1a9f41e5f63fe0ea
Ivgesha <evgenise93@gmail.com>
2021-05-01 22:23:22 +0300

    Preparations for working with Firebase + firestore

--- app/src/main/java/com/example/alarmlocation/MainActivity.java
+++ app/src/main/java/com/example/alarmlocation/MainActivity.java
@@ -6,6 +6,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // calculate between 2 points.
+    // todo: https://stackoverflow.com/questions/14394366/find-distance-between-two-points-on-map-using-google-map-api-v2
+    // work with firestore
+    // todo: https://www.youtube.com/watch?v=MILE4PVx1kE&ab_channel=CodinginFlow
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
